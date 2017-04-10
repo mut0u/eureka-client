eb4a1cd82dd183976bc2d8049b0f123e5fca745f
savior <michael.savior@gmail.com>
2017-04-10 16:18:17 +0800

    upgrade dependence

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://github.com/codebrickie/eureka-client"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-http "1.0.1" :exclusions [com.fasterxml.jackson.core/jackson-core]]
-                 [org.clojure/core.cache "0.6.4"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "2.3.0" :exclusions [com.fasterxml.jackson.core/jackson-core]]
+                 [org.clojure/core.cache "0.6.5"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
                    :plugins [[lein-midje "3.1.3"]
                              [codox "0.8.10"]]}})
