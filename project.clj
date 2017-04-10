826d556b50e9aa7c18f7ddaeab48dbb5b050d0a8
savior <michael.savior@gmail.com>
2017-04-10 21:15:26 +0800

    add remove instance function

--- project.clj
+++ project.clj
@@ -4,8 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-http "2.3.0" :exclusions [com.fasterxml.jackson.core/jackson-core]]
+                 [clj-http "3.4.1"
+                  ;;:exclusions [com.fasterxml.jackson.core/jackson-core]
+                  ]
+                 [cheshire "5.7.0"]
                  [org.clojure/core.cache "0.6.5"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]
-                   :plugins [[lein-midje "3.1.3"]
-                             [codox "0.8.10"]]}})
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]
+                   :plugins [[lein-midje "3.2.1"]
+                             [codox "0.10.3"]]}})
--- src/eureka_client/core.clj
+++ src/eureka_client/core.clj
@@ -1,12 +1,17 @@
 (ns eureka-client.core
   (:require [clj-http.client :as http]
             [clojure.set :refer [rename-keys]]
+            [cheshire.core :refer [parse-string]]
             [clojure.core.cache :as cache]))
 
 (def ^:private request-opts {:content-type :json
                              :socket-timeout 1000
                              :conn-timeout 1000
                              :accept :json})
+
+(defn parse-json [s]
+  (parse-string s (fn [k] (keyword k))))
+
 
 (def ^:private instances-cache (atom (cache/ttl-cache-factory {} :ttl 30000)))
 
@@ -40,6 +45,14 @@
        (map #(rename-keys % {:ipAddr :ip}))
        (map #(assoc % :port (read-string (:$ (:port %)))))))
 
+(defn- fetch-all-instances-from-server
+  [eureka-host eureka-port]
+  (->> (http/get (app-path eureka-host eureka-port "") (assoc request-opts :as :json))
+       :body
+       :applications
+       :application
+       ))
+
 (defn- find-instances-cached
   [eureka-host eureka-port app-id]
   (if (cache/has? @instances-cache app-id)
@@ -49,27 +62,53 @@
                         app-id
                         (fetch-instances-from-server eureka-host eureka-port app-id)))))
 
+(defn fetch-hostname []
+  (str (.getHostAddress (java.net.InetAddress/getLocalHost))))
+
 (defn register
   "Registers the app-id at the Eureka server and sends a heartbeat every 30
    seconds."
   [eureka-host eureka-port app-id own-port]
-  (let [host-id (str (.getHostAddress (java.net.InetAddress/getLocalHost)) "-" (rand-int 10000))]
+  (let [host-id (fetch-hostname)  ;;(str (.getHostAddress (java.net.InetAddress/getLocalHost)) "-" (rand-int 10000))
+        app-url (app-path eureka-host eureka-port app-id)]
     (http/post (app-path eureka-host eureka-port app-id)
-                 (assoc
-                   request-opts
-                   :form-params
-                   {:instance {:hostName host-id
-                               :app app-id
-                               :ipAddr (.getHostAddress (java.net.InetAddress/getLocalHost))
-                               :vipAddress app-id
-                               :status "UP"
-                               :port own-port
-                               :securePort 443
-                               :dataCenterInfo {:name "MyOwn"}}}))
-      (tick 30000 send-heartbeat eureka-host eureka-port app-id host-id)))
+               (assoc
+                request-opts
+                :form-params
+                {:instance {:hostName host-id
+                            :app app-id
+                            :ipAddr (.getHostAddress (java.net.InetAddress/getLocalHost))
+                            :vipAddress app-id
+                            :secureVipAddress app-id
+                            :status "UP"
+                            :port {:$ own-port (keyword "@enabled")  true}
+                            :securePort {:$ (str "8" own-port) (keyword "@enabled")  true}
+                            :healthCheckUrl (str app-url "/healthcheck"),
+                            :statusPageUrl (str app-url "/status"),
+                            :homePageUrl (str app-url "/home")
+                            :dataCenterInfo {(keyword "@class") "com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo" :name "MyOwn"}}}
+                ))
+    (tick 30000 send-heartbeat eureka-host eureka-port app-id host-id)))
+
+(defn remove-instance
+  "remvoe the instance by instanceid"
+  [eureka-host eureka-port app-id host-id]
+  (let [v (str (app-path eureka-host eureka-port app-id) "/" host-id)]
+    (->> (http/delete v)
+         :status
+         (= 200))))
 
 (defn find-instances
   "Finds all instances for a given app id registered with the Eureka server.
   Results are cached for 30 seconds."
   [eureka-host eureka-port app-id]
   (get (find-instances-cached eureka-host eureka-port app-id) app-id))
+
+
+(defn query-all-instances
+  [eureka-host eureka-port]
+  (->> (http/get (eureka-path eureka-host eureka-port) {:accept :json})
+       :body
+       parse-json
+       :applications
+       :application))
